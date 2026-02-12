c933d4b7c56ae9c8aff3e2b21098585cdcd1110d
mulugetadaniel74-design <mulugetadaniel74@gmail.com>
2026-02-12 20:17:04 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,13 +1,11 @@
-# መስመር 14 ላይ ያለውን በዚህ ተካው፡
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Daniel's Website</h1><p>Welcome to my first website!</p>"
+    return "<h1>Daniel's Website</h1><img src='https://github.com/mulugetadaniel74-design/my_website/blob/main/Screenshot_20260212_001355.png?raw=true' width='300'>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
-  
+    
