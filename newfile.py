a5487b5f75a1dc9c4f380802c3b7877c29ebfcd1
mulugetadaniel74-design <mulugetadaniel74@gmail.com>
2026-02-12 19:53:40 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,2 +1,13 @@
 # መስመር 14 ላይ ያለውን በዚህ ተካው፡
-app.run(host="0.0.0.0", port=10000)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Daniel's Website</h1><p>Welcome to my first website!</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+  
