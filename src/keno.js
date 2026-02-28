ff4706b0b06d661ba72178957c85a24e24d76f07
mulugetadaniel74-design <mulugetadaniel74@gmail.com>
2026-02-28 12:18:42 +0300

    Create keno.js

---
+++ src/keno.js
@@ -0,0 +1,20 @@
+const keno = {
+    play: function() {
+        alert("ጨዋታው ተጀምሯል!");
+    },
+    clearSelection: function() {
+        alert("ምርጫዎች ተጠርገዋል!");
+    },
+    quickPlay: function() {
+        alert("ፈጣን ጨዋታ!");
+    },
+    slowPlay: function() {
+        alert("ቀስ ያለ ጨዋታ!");
+    },
+    openPayoutsScreen: function() {
+        alert("የክፍያ ማያ ገጽ!");
+    },
+    closePayoutsScreen: function() {
+        alert("የክፍያ ማያ ገጽ ተዘግቷል!");
+    }
+};
